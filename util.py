a25435163cc34d71cc62d5109cae7eae21c4bbf2
Jasjeev <jasjeev99@gmail.com>
2025-02-05 12:35:28 +0530

    Add built-up area option to prediction form and implement standard scaling in price estimation

--- util.py
+++ util.py
@@ -1,12 +1,15 @@
 import json
 import pickle
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+
 
 __locations = None
 __data_columns = None
 __model = None
+__scalar = None
 
-def get_estimated_price(area_type, total_sqft, bath, balcony, availability_day, availability_month, size_bhk,location):
+def get_estimated_price(area_type, total_sqft, bath, balcony, availability_day, availability_month, size_bhk, location):
     try:
         loc_index = __data_columns.index(location.lower())
     except:
@@ -22,9 +25,11 @@ def get_estimated_price(area_type, total_sqft, bath, balcony, availability_day, 
     x[6] = size_bhk
     if loc_index >= 0:
         x[loc_index] = 1
-        
-    return round(__model.predict([x])[0],2)
+    
+    # Standard scaling the input features
+    x_scaled = __scalar.transform([x])  # Ensure __scaler is fitted beforehand
 
+    return round(__model.predict(x_scaled)[0], 2)
 def get_location_names():
     print("get_location_names called")
     return __locations
@@ -41,6 +46,10 @@ def load_saved_artifacts():
     global __model
     with open("bangalore_house_price_prediction_model.pickle",'rb') as f:
         __model = pickle.load(f)
+        
+    global __scalar
+    with open("scaler.pickle",'rb') as f:
+        __scalar = pickle.load(f)
         
     print("loading saved artifacts...done") 
     
